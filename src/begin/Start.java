32faacbee855c1e17af2c298b94660d008a51d08
AnnaSem22 <plombamexx@gmail.com>
2024-11-04 19:41:59 +0300

    Initial commit Update app info

--- src/begin/Start.java
+++ src/begin/Start.java
@@ -1,9 +1,16 @@
 package begin;
-
+/*
 public class Start {
 
         public static void main(String[] args) {
             System.out.println("This is shop-online app.");
         }
+
+}*/
+public class Start {
+
+    public static void main(String[] args) {
+        System.out.println("Shop-online for customers.");
+    }
 
 }
